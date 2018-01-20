4d287800dfe591069e41ee7b9f367521904a3cbe
meofthepresent <sniper96a@gmail.com>
2018-01-19 22:26:34 -0500

    finished basic draft

--- PSOCL.h
+++ PSOCL.h
@@ -17,23 +17,31 @@ typedef struct swarm {
 } swarm;
 
 //struct for the opencl accelerated swarm
-typedef struct swarm {
+typedef struct clswarm {
     int partnum, dimnum;
     double *gbest;
     particle *herd;
     cl_device_id device_id;
     cl_context context;
     cl_command_queue command_queue;
-} swarm;
+} clswarm;
 
+//swarm initializaion
 void initswarm(char type, int dimensionnum, int partnum, swarm school);
 
 void clinitswarm(char type, int dimensionnum, int partnum, clswarm school);
 
+//swarm particle distribution
 void distributeparticles(swarm school);
 
 void cldistributeparticles(clswarm school);
 
+//running the swarm
+void clrunswarm(int iterations, clswarm school, double (*fitness)(particle *);
+
+void runswarm(int iterations, swarm school,double (*fitness)(particle *));
+
+//releasing the swarm
 void releaseswarm(swarm school);
 
 void clreleaseswarm(clswarm school);
