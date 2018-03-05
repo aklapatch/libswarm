8249af48fbc534fe8aad69a956053cf16345c961
meofthepresent <sniper96a@gmail.com>
2018-03-05 01:29:22 -0500

    modified test cl

--- tests/allocspeedtest.c
+++ tests/allocspeedtest.c
@@ -42,7 +42,7 @@ int main() {
 	
 	///get device info
 	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);	
-	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &ret_num_devices);
 	
 	///compile kernel and get context
 	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
--- tests/test.cl
+++ tests/test.cl
@@ -1,11 +1,11 @@
 __kernel void add(__global float * result, __global int * num,__global float * data){
 	*result=0;
-	int i;
-	for(i=0;i<*num;++i){
-		*result+=data[i];
+	int i,j;
+	///just enought to see the gpu is being used
+	for(j=0;j<400000;++j){
+		for(i=0;i<*num;++i){
+			*result+=data[i];
+		}
 	}
-	while(*num){
-		*result+=data[*num];
-		num[0]=num[0]-1;
-	}
+	
 }
