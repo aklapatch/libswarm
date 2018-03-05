81c8742baf1cb2085dfe1be0248257932655a3d2
meofthepresent <sniper96a@gmail.com>
2018-03-05 00:27:59 -0500

    finished opencl tests

--- tests/allocspeedtest.c
+++ tests/allocspeedtest.c
@@ -2,26 +2,111 @@
 ///if you should allocate memory on the GPU instead.
 
 #include <CL/cl.h>
+#include<stdio.h>
+#include<time.h>
+
+#define SIZE (0x100000)	
+#define PRINTRET printf("ret= %d\n", ret);
 
 int main() {
-	///random data
-	cl_float data[]={6,3,2,345,3,45,3,45,5,2,4,5,42,4,5};
+	//CPU data
+	float data[]={6,3,2,345,3,45,3,45.3,5,2.5,4,5.6,42.3,4.2,5};
+	cl_int num=15;
+	float result=13.1;
 	
+	//GPU buffers
+	cl_mem numbuf = NULL;	
+	cl_mem databuf = NULL;	
+	cl_mem resbuffer = NULL;
 	
+	//basic opencl information
 	cl_platform_id platform_id = NULL;
 	cl_device_id device_id = NULL;
 	cl_context context = NULL;
 	cl_command_queue command_queue = NULL;
-	
-	cl_mem data = NULL;	
-	cl_float result;
-	
 	cl_program program = NULL;
 	cl_kernel kernel = NULL;	
 	cl_uint ret_num_devices;
 	cl_uint ret_num_platforms;
-	cl_int ret;
+	cl_int ret;	
 	
-	///result data and input data
-	cl_mem data = NULL;	
-	cl_float result;
+	///open kernel
+	FILE * fp = fopen("test.cl", "r");
+	if(!fp){
+		fprintf(stderr, "Failed to load kernel.\n");	
+		exit(1);
+	}	
+	char * src=(char*)malloc(SIZE);
+	size_t src_size=fread(src, 1, SIZE, fp);
+	fclose(fp);
+	
+	///get device info
+	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);	
+	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+	
+	///compile kernel and get context
+	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+	command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+	
+	program = clCreateProgramWithSource(context, 1, (const char **)&src, (const size_t *)&src_size, &ret);
+		
+	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+	
+	kernel = clCreateKernel(program, "add", &ret);
+	
+	
+	
+	///make result buffer
+	resbuffer=clCreateBuffer(context, CL_MEM_READ_WRITE,sizeof(float), NULL, &ret);
+	ret = clEnqueueWriteBuffer(command_queue, resbuffer, CL_TRUE, 0, sizeof(float), &result, 0, NULL, NULL);
+	
+	///write number data to gpu
+	numbuf=clCreateBuffer(context, CL_MEM_READ_WRITE,sizeof(int), NULL, &ret);
+	ret = clEnqueueWriteBuffer(command_queue, numbuf, CL_TRUE, 0, sizeof(int), &num, 0, NULL, NULL);
+	
+	//write data buffer to gpu
+	databuf=clCreateBuffer(context, CL_MEM_READ_WRITE,num*sizeof(float), NULL, &ret);
+	ret = clEnqueueWriteBuffer(command_queue, databuf, CL_TRUE, 0, num*sizeof(float), &data, 0, NULL, NULL);
+	
+	PRINTRET;
+	
+	///set kernel args
+	ret=clSetKernelArg(kernel,0,sizeof(cl_mem), (void *)&resbuffer);
+	PRINTRET;
+	ret=clSetKernelArg(kernel,1,sizeof(cl_mem),&numbuf);
+	PRINTRET;
+	ret=clSetKernelArg(kernel,2,sizeof(cl_mem), (void *)&databuf);
+	PRINTRET;
+	
+	
+	
+	///execute
+	ret = clEnqueueTask(command_queue, kernel, 0, NULL,NULL);
+	
+	PRINTRET;
+	
+	printf("resultbefore= %f\n", result);
+	
+	//read back result
+	ret = clEnqueueReadBuffer(command_queue, resbuffer, CL_TRUE, 0,sizeof(float), &result, 0, NULL, NULL);
+	
+	PRINTRET;
+	
+	printf("result= %f\n", result);
+	
+	ret = clFlush(command_queue);
+	ret = clFinish(command_queue);	
+	ret = clReleaseKernel(kernel);
+	ret = clReleaseProgram(program);
+	ret = clReleaseMemObject(resbuffer);
+	ret = clReleaseMemObject(databuf);
+	ret = clReleaseMemObject(numbuf);	
+	ret = clReleaseCommandQueue(command_queue);
+	ret = clReleaseContext(context);
+	free(src);
+	
+	return 0;
+}
+	
+	
+	
