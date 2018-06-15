5c7545665ae7c942d7c4f097b4fd5e7daaf8e96f
Aaron Klapatch <sniper96a@gmail.com>
2018-06-15 10:13:43 -0400

    Update clswarm.hxx

--- src/clswarm.hxx
+++ src/clswarm.hxx
@@ -56,29 +56,29 @@ void printbuf(cl::Buffer buf, size_t size,cl::CommandQueue q){
 class clswarm {
 	private:
 	
-		// used for debugging and error checking
-		cl_int ret;
+	// used for debugging and error checking
+	cl_int ret;
 
-        /// Number of particles and Number of dimensions for each particle
+        // Number of particles and Number of dimensions for each particle
         cl_uint partnum, dimnum;
         
-        ///buffers for the particle number and dimension numbers
+        //buffers for the particle number and dimension numbers
         cl::Buffer dimnumbuf, partnumbuf;
 
-        ///best particle dimensions, its fitness, swarm bounds
+        //best particle dimensions, its fitness, swarm bounds
         cl::Buffer gbestbuf, upperboundbuf, lowerboundbuf;
 
-        ///set that so all fitness numbers will show up
+        //set that so all fitness numbers will show up
         cl::Buffer gfitbuf;
 
-        ///inertial weight and 2 behavioral constants
+        //inertial weight and 2 behavioral constants
         cl::Buffer wbuf, c1buf, c2buf;
 
-        ///particle data
+        //particle data
         cl::Buffer presentbuf, pbestbuf, vbuf;
         cl::Buffer pfitnessbuf, fitnessbuf;
 
-		///all the opencl stuff
+	//all the opencl stuff
         std::vector<cl::Platform> platforms;
         std::vector<cl::Device> devices;
         cl::Context context;
@@ -88,62 +88,76 @@ class clswarm {
         cl::Kernel distr, cmpre, updte, updte2;
 
     public:
-        ///defaults to 100 particles and 1 dimension
+        /// Default constructor.
+        /// Makes a swarm with 100 particles and a 1 dimension problem space.
         clswarm();
 
-        ///sets no. particles and no. dimensions and w
-        clswarm(cl_uint, cl_uint,cl_float,cl_float,cl_float);
+        /** Makes a Swarm with the specified parameters.
+         * @param numparts Number of particles in the swarm.
+         * @param numdims Number of dimensions in problem space.
+         * @param w How much velocity carries over from update to update. Best left between 0 and 1.
+         * @param c1 How much the particle's best position affects the particle's velocity.
+         * @param c2 How much the global best affects the individual particle's velocity.
+         */
+        clswarm(cl_uint numparts, cl_uint numdims,cl_float w,cl_float c1,cl_float c2);
 
-        ///frees all swarm memory
+        /// Destructor.
+        /// Frees Heap-allocated memory
         ~clswarm();
 
-        ///sets the number of particles
+        /// Sets the number of particles in the swarm.
+        /// Deletes all stored particle data
         void setPartNum(cl_uint);
 
-		///get particle numbers
+        /// Returns number of particles in the swarm.
         cl_uint getPartNum();
 
-        ///sets no. of dimensions
+        /// Sets number of dimensions in the problem space.
+        /// Deletes all particle data
         void setDimNum(cl_uint);
 
-		///get dimension number
-		cl_uint getDimNum();
+        /// Returns number of dimensions in problem space
+	cl_uint getDimNum();
 
-        ///sets inertial weight
+     	/// Sets the inertial weight.
         void setWeight(cl_float);
 
-		///returns inertial weight
-		cl_float getWeight();
+	/// Returns the inertial weight.
+	cl_float getWeight();
 
-		void setC1(cl_float);
+	/// Sets the constant affecting the pull of particles' best.
+	void setC1(cl_float);
 
-		///returns the first behavioral constant
-		cl_float getC1();
+        /// Returns behavioral constant that determines the pull of the particle's best.
+	cl_float getC1();
 		
-
-		void setC2(cl_float);
+        ///  Sets the constant affecting the pull of the global best.
+	void setC2(cl_float);
 		
-		///returns the second behavioral constant
-		cl_float getC2();
+	/// Returns global best behavioral constant.
+	cl_float getC2();
 
-        /// sets upper and lower bounds and distributes particles linearly between them
-        /** The lower bound is first argument, upper bound is second argument */
-        void distribute(cl_float * , cl_float *);
+        /** Sets upper and lower bounds for the problem space and distributes linearly between them.
+         * @param lowerbound The lower bound in the problemspace.
+         * @param upperbound The upper bound in the problem space.
+         */ 
+        void distribute(cl_float * upperbound , cl_float * lowerbound);
 
-        /// updates number of times with *fitness as a fitness function
-        void update(unsigned int);
+        /** Updates all particle data values
+         * @param times The number of times to update the particle values.
+         */
+        void update(unsigned int times);
 
+	/// Copies data from the input argument into the particle array.
+	void setPartData(cl_float *);
 
-		///sets particle data
-		void setPartData(cl_float *);
-
-        ///returns all particle data
+        /// Copies particle data into input argument.
         void getPartData(cl_float *);
 
         ///returns the fitness of the best particle
         cl_float getGFitness();
 		
-		///returns the best position in the swarm.
+	/// Copies particle data into the input argument.
         void getGBest(cl_float *);
 };
 #endif  //CLSWARM_HXX
